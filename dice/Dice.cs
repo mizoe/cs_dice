5632e46d3f9abe197666d27e59306cc5fcf5495c
Tomo Mizoe <mz@july.co.jp>
2015-12-07 17:35:52 +0900

    問49 統計情報: 各目の回数

--- dice/Dice.cs
+++ dice/Dice.cs
@@ -12,18 +12,26 @@ namespace dice
     {
         private Random rand = new Random();
         private List<int> history = new List<int>();
+        private int[] stat = new int[7];
+
         public int Roll()
         {
             int result = rand.Next(1, 7);
             history.Add(result);
+            stat[result]++;
             return result;
         }
         public string getHisotry()
         {
-            string msg = "";
+            string msg = "--- 履歴 ---\n";
             foreach (var item in history)
             {
                 msg += " " + item;
+            }
+            msg += "\n--- 統計 ---\n";
+            for (int i = 1; i < 7; i++)
+            {
+                msg += i + " : " + stat[i] + "回\n";
             }
             return msg;
         }
