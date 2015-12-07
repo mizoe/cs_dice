75748dfa212830a9a6092dac9ab7b10f2b5ec503
Tomo Mizoe <mz@july.co.jp>
2015-12-07 16:19:40 +0900

    必ずゼロを返すサイコロ

---
+++ dice/Dice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice
+{
+    class Dice
+    {
+        public int roll()
+        {
+            return 0;
+        }
+    }
+}
---
+++ dice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice = new Dice();
+            Console.WriteLine(dice.roll());
+        }
+    }
+}
